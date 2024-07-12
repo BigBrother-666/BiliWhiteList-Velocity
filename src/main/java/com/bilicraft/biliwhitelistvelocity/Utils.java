e064ce9a50592229df0eae8431988dba142a4ba1
dyh <2361624529@qq.com>
2024-07-12 09:58:25 +0800

    dev: 合并utils

---
+++ src/main/java/com/bilicraft/biliwhitelistvelocity/Utils.java
@@ -0,0 +1,20 @@
+package com.bilicraft.biliwhitelistvelocity;
+
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+
+public class Utils {
+    public static TextComponent coloredMessage(String msg) {
+        return LegacyComponentSerializer.legacyAmpersand().deserialize(msg);
+    }
+
+    public static boolean boolFromInt(int i){
+        return i != 0;
+    }
+
+    public static int boolToInt(boolean bool){
+        if(bool)
+            return 1;
+        return 0;
+    }
+}
