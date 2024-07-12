3d1b50bffb2bc32344257aeeed45b2a08058d88e
dyh <2361624529@qq.com>
2024-07-12 17:56:25 +0800

    bug fix: 无白名单玩家不能进入无需白名单的服务器

--- src/main/java/com/bilicraft/biliwhitelistvelocity/listeners/JoinListener.java
+++ src/main/java/com/bilicraft/biliwhitelistvelocity/listeners/JoinListener.java
@@ -1,18 +1,15 @@
 package com.bilicraft.biliwhitelistvelocity.listeners;
 
 import com.bilicraft.biliwhitelistvelocity.BiliWhiteListVelocity;
-import com.bilicraft.biliwhitelistvelocity.Utils;
+import com.bilicraft.biliwhitelistvelocity.common.Utils;
 import com.bilicraft.biliwhitelistvelocity.config.Config;
 import com.velocitypowered.api.event.PostOrder;
-import com.velocitypowered.api.event.ResultedEvent;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.connection.PreLoginEvent;
 import com.velocitypowered.api.event.player.ServerPreConnectEvent;
-import com.velocitypowered.api.proxy.ServerConnection;
-import com.velocitypowered.api.proxy.server.RegisteredServer;
+import com.bilicraft.biliwhitelistvelocity.manager.WhiteListManager;
 import net.kyori.adventure.text.TextComponent;
 import org.enginehub.squirrelid.Profile;
-import com.bilicraft.biliwhitelistvelocity.manager.WhiteListManager;
 
 import java.util.Map;
 import java.util.HashMap;
@@ -38,57 +35,32 @@ public class JoinListener {
             plugin.getLogger().info("玩家 {} 不是正版 Minecraft 账号，已拒绝", playerName);
             return;
         }
-        plugin.getCache().put(new Profile(playerUniqueId, playerName));
-        // 获取玩家加入的服务器
-        String forcedHost = getForcedHost(event.getConnection().getVirtualHost().get().getHostString());
-//        String serverName = null;
-//        if (server != null) {
-//            serverName = server.getServerInfo().getName();
-//        } else {
-//            // test
-//            plugin.getLogger().error("玩家 {} # {} 连接服务器错误：server=null", playerName, playerUniqueId);
-//        }
-
-        if (!plugin.getWhiteListManager().isSeverRequireWhiteList(forcedHost)) {
-            plugin.getLogger().info("玩家 {} # {} 例外列表放行：{}", playerName, playerUniqueId, forcedHost);
-            return;
-        }
-        WhiteListManager.RecordStatus status = plugin.getWhiteListManager().checkWhiteList(playerUniqueId);
-        if (status != WhiteListManager.RecordStatus.WHITELISTED) {
-            TextComponent kickMessage = Utils.coloredMessage((String) messages.get("no-whitelist"));
-            event.setResult(PreLoginEvent.PreLoginComponentResult.denied(kickMessage));
-            plugin.getLogger().info("玩家 {} # {} 没有白名单，已拒绝", playerName, playerUniqueId);
-        } else {
-            plugin.getLogger().info("玩家 {} # {} 白名单放行", playerName, playerUniqueId);
-        }
-    }
-
-    private Map<String, String> getForcedHosts() {
-        Map<String, String> hosts = new HashMap<>();
-        for (Map.Entry<String, String> host : plugin.getServer().getConfiguration().getServers().entrySet()) {
-            hosts.put(host.getKey().toLowerCase(), host.getValue().toLowerCase());
-        }
-        return hosts;
-    }
-
-    private String getForcedHost(String virtualHost) { //Maybe null
-        return getForcedHosts().get(virtualHost);
     }
 
     @Subscribe(order = PostOrder.EARLY)
     public void onServerSwitch(ServerPreConnectEvent event) {
         String username = event.getPlayer().getUsername();
+        UUID uniqueId = event.getPlayer().getUniqueId();
+        String previousServerName = null;
+        if (event.getPreviousServer() != null) {
+            previousServerName = event.getPreviousServer().getServerInfo().getName();
+        }
         String targetServerName = event.getOriginalServer().getServerInfo().getName();
         if (!plugin.getWhiteListManager().isSeverRequireWhiteList(targetServerName)) {
-            plugin.getLogger().info("玩家 {} # {} 例外列表放行： {}", username, event.getPlayer().getUniqueId(), targetServerName);
+            plugin.getLogger().info("玩家 {} # {} 例外列表放行：{}", username, uniqueId, targetServerName);
             return;
         }
-        WhiteListManager.RecordStatus status = plugin.getWhiteListManager().checkWhiteList(event.getPlayer().getUniqueId());
+        WhiteListManager.RecordStatus status = plugin.getWhiteListManager().checkWhiteList(uniqueId);
         if (status != WhiteListManager.RecordStatus.WHITELISTED) {
             event.setResult(ServerPreConnectEvent.ServerResult.denied());
-            event.getPlayer().sendMessage(Utils.coloredMessage(((String) messages.get("no-whitelist-switch")).replace("{server}", targetServerName)));
+            if (previousServerName != null) {
+                event.getPlayer().sendMessage(Utils.coloredMessage(((String) messages.get("no-whitelist-switch")).replace("{server}", targetServerName)));
+            } else {
+                event.getPlayer().disconnect(Utils.coloredMessage((String) messages.get("no-whitelist")));
+                plugin.getLogger().info("玩家 {} # {} 没有白名单，已拒绝", username, uniqueId);
+            }
         } else {
-            plugin.getLogger().info("玩家 {} # {} 白名单放行： {}", username, event.getPlayer().getUniqueId(), targetServerName);
+            plugin.getLogger().info("玩家 {} # {} 白名单放行：{}", username, uniqueId, targetServerName);
         }
     }
 }
