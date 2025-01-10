eefa82258cd936d2507a0a9f3fd458d059c27bf9
dyh <2361624529@qq.com>
2025-01-10 16:02:20 +0800

    feat: 新增连坐惩罚确认

--- src/main/java/com/bilicraft/biliwhitelistvelocity/listeners/LiteBansListener.java
+++ src/main/java/com/bilicraft/biliwhitelistvelocity/listeners/LiteBansListener.java
@@ -9,6 +9,10 @@ import com.velocitypowered.api.proxy.Player;
 import com.velocitypowered.api.proxy.ProxyServer;
 import litebans.api.Entry;
 import litebans.api.Events;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.event.ClickEvent;
+import net.kyori.adventure.text.event.HoverEvent;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.enginehub.squirrelid.Profile;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -36,12 +40,37 @@ public class LiteBansListener extends Events.Listener {
                     String[] ret = getInviter(entry.getUuid(), entry.getExecutorUUID());
                     if (ret == null) {
                         break;
+                    } else {
+                        CommandSource commandSource = getCommandSource(entry.getExecutorUUID());
+                        if (commandSource == null) {
+                            return;
+                        } else if (commandSource instanceof Player executor) {
+                            // 获取邀请人和被邀请人信息
+                            String inviterUuid = ret[0];
+                            String inviterName = ret[1];
+                            String inviteeName = ret[2];
+
+                            if ((Boolean) Config.getJointLiabilityConf().getOrDefault("auto", false)) {
+                                punish(inviterUuid, inviterName, inviteeName, executor);
+                                return;
+                            }
+
+                            // 发送手动确认信息
+                            @SuppressWarnings("unchecked")
+                            List<String> cmd = (List<String>) Config.getJointLiabilityConf().getOrDefault("inviter-punishment", Collections.emptyList());
+                            Component hoverText = Component.text("将要执行的处罚指令：\n");
+                            for (String s : cmd) {
+                                s = s.replace("{inviter}", inviterName).replace("{invitee}", inviteeName).trim();
+                                hoverText = hoverText.append(Component.text(s + "\n", NamedTextColor.RED));
+                            }
+                            Component confirmText = Component.text("[ 确认对上级邀请人执行连带处罚 ]", NamedTextColor.RED);
+                            confirmText = confirmText.hoverEvent(HoverEvent.showText(hoverText));
+                            confirmText = confirmText.clickEvent(ClickEvent.callback(audience -> punish(inviterUuid, inviterName, inviteeName, executor)));
+                            executor.sendMessage(confirmText);
+                        } else {
+                            commandSource.sendMessage(Component.text("控制台不支持自动执行连带处罚，请手动执行", NamedTextColor.YELLOW));
+                        }
                     }
-                    String inviterUuid = ret[0];
-                    String inviterName = ret[1];
-                    String inviteeName = ret[2];
-                    // 符合连带处罚条件
-                    punish(inviterUuid, inviterName, inviteeName, entry.getExecutorUUID());
                 }
                 break;
             case "mute":
@@ -61,40 +90,38 @@ public class LiteBansListener extends Events.Listener {
 
     }
 
-    private void punish(String inviterUuid, String inviterName, String inviteeName, String executorUUID) {
-        CommandSource commandSource = getCommandSource(executorUUID);
-        if (commandSource == null) {
-            return;
-        }
+    private void punish(String inviterUuid, String inviterName, String inviteeName, Player executor) {
         // 检查inviter是否在排除列表中
         @SuppressWarnings("unchecked")
         List<String> whitelist = (List<String>) Config.getJointLiabilityConf().getOrDefault("inviter-whitelist", Collections.emptyList());
+
         if (whitelist != null && whitelist.contains(inviterUuid)) {
             // 不处罚
-            commandSource.sendMessage(Utils.coloredMessage("&e " + inviterName + " &a在连带处罚白名单中，不进行处罚"));
+            executor.sendMessage(Utils.coloredMessage("&e " + inviterName + " &a在连带处罚白名单中，不进行处罚"));
             return;
         }
 
         // 执行处罚指令
         @SuppressWarnings("unchecked")
         List<String> cmd = (List<String>) Config.getJointLiabilityConf().getOrDefault("inviter-punishment", Collections.emptyList());
-        if (cmd != null) {
-            ProxyServer server = plugin.getServer();
-            for (String s : cmd) {
-                s = s.replace("{inviter}", inviterName).replace("{invitee}", inviteeName).trim();
-                if (commandSource instanceof Player player && player.isActive() && !s.startsWith("litebans")) {
-                    // 非litebans指令由玩家身份执行（如果ban指令由玩家执行）
-                    player.spoofChatInput("/" + s);
-                    continue;
-                }
-                if (s.startsWith("litebans")) {
-                    server.getCommandManager().executeAsync(commandSource, s);
-                } else {
-                    commandSource.sendMessage(Utils.coloredMessage("&c因为在控制台执行指令，跳过执行惩罚：" + s));
-                }
+        int count = 0;
+        ProxyServer server = plugin.getServer();
+
+        for (String s : cmd) {
+            s = s.replace("{inviter}", inviterName).replace("{invitee}", inviteeName).trim();
+            if (executor.isActive() && !s.startsWith("litebans")) {
+                // 非litebans指令由玩家身份执行（如果ban指令由玩家执行）
+                executor.spoofChatInput("/" + s);
+                count++;
+                continue;
+            }
+            if (executor.isActive() && s.startsWith("litebans")) {
+                server.getCommandManager().executeAsync(executor, s);
+                count++;
             }
         }
-        commandSource.sendMessage(Utils.coloredMessage("&a连带处罚执行完成"));
+
+        executor.sendMessage(Utils.coloredMessage("&a连带处罚执行完成，执行了 %d 条指令".formatted(count)));
     }
 
     private String[] getInviter(String inviteeUuid, String executorUUID) {
@@ -125,8 +152,8 @@ public class LiteBansListener extends Events.Listener {
                 source.sendMessage(Utils.coloredMessage("&c" + profile.getName() + "无人邀请、为虚拟玩家或者网络故障"));
                 return null;
             }
-            source.sendMessage(Utils.coloredMessage("&a " + profile.getName() + " 的上级邀请人为: &e" + inviter.getName()));
-            return new String[]{inviter.getUniqueId().toString().replace("-", ""), inviter.getName(), profile.getName()};
+            source.sendMessage(Utils.coloredMessage("&a " + profile.getName() + " 的上级邀请人为: &e%s &8(%s)".formatted(inviter.getName(), inviter.getUniqueId().toString())));
+            return new String[]{inviter.getUniqueId().toString(), inviter.getName(), profile.getName()};
         } catch (IOException | InterruptedException exception) {
             source.sendMessage(Utils.coloredMessage("&c查询上级邀请人时发生内部错误。错误代码：&7" + exception.getMessage()));
             return null;
