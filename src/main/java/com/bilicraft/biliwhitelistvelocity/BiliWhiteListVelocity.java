f7c9400e099af9459ba8829a1f99940a89b02a5c
dyh <2361624529@qq.com>
2025-01-10 17:55:17 +0800

    release 2.0

--- src/main/java/com/bilicraft/biliwhitelistvelocity/BiliWhiteListVelocity.java
+++ src/main/java/com/bilicraft/biliwhitelistvelocity/BiliWhiteListVelocity.java
@@ -36,7 +36,7 @@ import java.util.Map;
 @Plugin(
         id = "biliwhitelist-velocity",
         name = "BiliWhiteList-Velocity",
-        version = "1.0-SNAPSHOT",
+        version = "2.0",
         description = "Velocity version of BiliWhiteList plugin",
         authors = {"Ghost_chu", "BlackFoxSAR", "BigBrother"},
         dependencies = {
